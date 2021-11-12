51035410d3612276f7086afd82043fff03917f51
yannis van steen <yannis.van.steen@student.ehb.be>
2021-11-12 19:19:30 +0100

    fix:  Made the code follow the naming guidlines

--- api/src/index.js
+++ api/src/index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5543;
 const bgRouter = express.Router();
 
 // Makes connection with the postgress database in docker
-// console.log(process.env)
 const pg = require('knex')({
 
     client: 'pg',
@@ -21,37 +20,26 @@ const pg = require('knex')({
 });
 
 // Middleware
-const { request } = require('http');
-
 
 bgRouter.route('/users')
     .get((req, res) => {
-        CreatePostgressData();
-        // this code is temporrary to test my route
-        // let data = {
-        //     naam: "poggy"
-        // }
-        // res.send(data)
-        // RecievePostgressData();
-
-        // RecieveData(req,res)
+        createPostgressData();
     })
  
 bgRouter.route('/updateUser/:id')
     .patch((req, res) => {
-        //UpdateUsers(req, res);     
 });
 
 bgRouter.route('/deleteUser/:id')
     .delete((req, res) => {
-        //deleteUser(res,res);
 });
 
-function Startexpress() {
+
+function startexpress() {
     app.use('/api', bgRouter);
 
     app.get('/', (req, res) => {
-        RecievePostgressData().then((data) => {
+        recievePostgressData().then((data) => {
             console.log(data);
             res.send("data recieved");
         });
@@ -63,25 +51,18 @@ function Startexpress() {
     });
 }
 async function initTables() {
-
-    await CreateTable();
-    await CreatePostgressData();
+    await createTable();
+    await createPostgressData();
 }
+
 initTables();
-Startexpress();
+startexpress();
 
 
-//exports variables to the test js file
-module.exports = {
-    port,
-    app
-}
+// Postgress functions 
 
-// Functions
-
-// Calls postgress database in docker to recieve database data from the table "users"
-async function CreateTable() {
-    
+// Creates a table or checks if there is already a table with that name
+async function createTable() {
     await pg.schema.hasTable('users').then(function(exists) {
         if (!exists) {
           return pg.schema.createTable('users', function(t) {
@@ -91,65 +72,35 @@ async function CreateTable() {
         }
       });
 }
-async function CreatePostgressData() {
+
+// Creates a row inside the users table
+async function createPostgressData() {
     await pg.table('users').insert({naam: "test"})
 }
 
-async function RecievePostgressData() {
+// Recieve data from the users table
+async function recievePostgressData() {
     return await pg.select('naam').from('users')
-    // await pg.select().table("users");
+}
+
+// Update a row inside the users table
+async function updatePostgressData() {
+    return await pg.select('naam').from('users')
+}
+
+// Delete a row inside the users table
+async function deletePostgressData() {
+    return await pg.select('naam').from('users')
 }
 
 
 
-//Not Postgress---------------
-// Calls to a pgadmin application database to getdata (not dockerised)
-function RecieveData(req, res) {
-    client.query(`Select * from users`, (err, res) => {
-        if (!err) {
-            console.log(res.rows);
-        } else {
-            console.log(err.message);
-        }
-    });
-    res.send("succesfully read");
-}
-
-// Calls to a pgadmin application database to delete data (not dockerised)
-async function deleteUser(req,res) {
-    await client.query(
-        `DELETE FROM public.users WHERE "id" = '${req.params.id}';`,
-        (err, res) => {
-            console.log(err, res);
-        }
-    );
-    res.send("succefully deleted!")
-}
-
-// Calls to a pgadmin application database to update data (not dockerised)
-async function UpdateUsers(req, res) {
-    request.post
-    await client.query(
-        `UPDATE public.users
-        SET "naam" = '${req.body.naam}'
-        WHERE "id" = '${req.params.id}'`,
-        (err, res) => {
-            console.log(err, res);
-        }
-    );
-    res.send("Succesfully updated!")
+//exports variables to the test js file
+module.exports = {
+    port,
+    app
 }
 
 
-// post route die een row aanmaakt in de database (not dockerised)
-// app.post('/addUser', (req, res) => {
-//     client.query(
-//         "INSERT INTO users(id, naam)VALUES(gen_random_uuid(), 'Jeff')",
-//         (err, res) => {
-//             console.log(err, res);
-//             client.end();
-//         }
-//     );
-//     res.send("Succesfully added")
-// });
+// gen_random_uuid()
 
